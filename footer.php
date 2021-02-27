3d8607938310e22a8e907fbe2e4c34a509cd362a
ZTomova <52647110+ZTomova@users.noreply.github.com>
2021-02-27 23:07:21 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p>Here is the place where you can rent your dream car for as long as you want. Enjoy!</p>
+					<ul>
+						<li><a href="http://facebook.com/rentacar" class="facebook" target="_blank"></a></li>
+						<li><a href="http://twitter.com/rentacar" class="twitter" target="_blank"></a></li>
+						<li><a href="http://plus.google.com/rentacar" class="google" target="_blank"></a></li>
+						
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright ;  All Rights Reserved.
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
