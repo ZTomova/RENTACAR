84324c1ecd5810feb085bdfc6365c366bc0a782f
ZTomova <52647110+ZTomova@users.noreply.github.com>
2021-02-28 11:20:28 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,26 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Rentacar Dream</title>
-	<meta charset="utf-8">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
-	
-	<link rel="stylesheet" type="text/css" href="css/reset.css">
-	
-
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
-</head>
-<body>
-
-	<section class="">
-		<?php
-			include 'header.php';
-		?>
-
-			<section class="caption">
-				<h2 class="caption" style="text-align: center">Find Your Dream Car and Ren It Now</h2>
-				<h3 class="properties" style="text-align: center">We have all that you are looking for</h3>
-			</section>
-	</section>
+<?php
+	session_start();
+	error_reporting("E-NOTICE");
+?>
+<header>
+			<div class="wrapper">
+				<h1 class="logo"> Car Company</h1>
+			
+				<nav>
+					<?php
+						if(!$_SESSION['email'] && (!$_SESSION['pass'])){
+					?>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="index.php">Rent Cars</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contacts</a></li>
+					</ul>
+					<a href="account.php">Client Login</a>
+					<a href="login.php">Admin Login</a>
+					<?php
+						} else{
+					?>
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="status.php">View Status</a></li>
+							</ul>
+					<a href="admin/logout.php">Logout</a>
+					<?php
+						}
+					?>
+				</nav>
+			</div>
+		</header>
